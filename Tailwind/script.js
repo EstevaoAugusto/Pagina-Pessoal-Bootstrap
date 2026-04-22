276b99488a94127489b4696bf5e1fdcac10207ee
Estevão Augusto <estevaoaugusto2005@gmail.com>
2026-04-21 21:07:35 -0300

    feat: atualiza versao Tailwind

--- Tailwind/script.js
+++ Tailwind/script.js
@@ -33,3 +33,40 @@ function handleSubmit(e) {
   document.getElementById('successMsg').classList.remove('hidden');
   e.target.reset();
 }
+
+// -- Menu Hamburger --
+const menuBtn = document.getElementById('menuBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+const mobileLinks = document.querySelectorAll('.mobile-link');
+const allSections = document.querySelectorAll('section');
+const navBar = document.querySelector('nav');
+const logoHero = document.getElementById('logoHero');
+
+function toggleMenu() {
+    // Abre/Fecha o menu
+    mobileMenu.classList.toggle('translate-x-full');
+   
+
+
+    // Animação do ícone Hambúrguer vira "X"
+    line1.classList.toggle('rotate-[42deg]');
+    line2.classList.toggle('opacity-0');
+    line3.classList.toggle('-rotate-[42deg]');
+    
+    // Bloqueia o scroll do corpo quando o menu está aberto
+    document.body.classList.toggle('overflow-hidden');
+
+    allSections.forEach(section => {
+        section.classList.toggle('blur-sm');
+    });
+  }
+
+menuBtn.addEventListener('click', toggleMenu);
+
+// Fecha o menu ao clicar em qualquer link
+mobileLinks.forEach(link => {
+  link.addEventListener('click', toggleMenu);
+});
